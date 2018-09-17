16eb70fc0ea6d9db3a30e32b5c4822d4732b0460
Avi Aryan <avi.aryan123@gmail.com>
2018-09-17 22:29:12 +0530

    check for empty strings in contract

--- contracts/promisebox.sol
+++ contracts/promisebox.sol
@@ -38,6 +38,7 @@ contract PromiseBox {
     /// Save your word to the blockchain
     function addWord(string text) public payable {
         require(msg.value >= min_bet, "Bet should be more than 0.001 eth");
+        require(utfStringLength(text) > 0, "Text cannot be empty");
         words[getID()] = Word({
             text: text,
             bet: msg.value,
@@ -66,6 +67,7 @@ contract PromiseBox {
     /// Set a nickname for yourself
     function setNick(string name) public payable {
         require(msg.value >= nick_price, "Insufficient fees for setting nick");
+        require(utfStringLength(name) > 0, "Name cannot be empty");
         names[msg.sender] = name;
         treasure.transfer(msg.value);
     }
@@ -76,9 +78,9 @@ contract PromiseBox {
     }
 
     /// Returns word by a given ID
-    function getWordById(uint id) public view returns(string, address, bool, bool) {
+    function getWordById(uint id) public view returns(string, address, uint, bool, bool) {
         Word memory word = words[id];
-        return (word.text, word.owner, word.resolved, word.verdict);
+        return (word.text, word.owner, word.bet, word.resolved, word.verdict);
     }
 
     /// Returns the total number of words saved in the contract
@@ -94,5 +96,28 @@ contract PromiseBox {
     // private functions
     function getID() private returns(uint) {
         return counter++;
+    }
+
+    /* HELPERS */
+    // https://ethereum.stackexchange.com/questions/13862/
+    function utfStringLength(string str) internal pure returns (uint) {
+        uint i = 0;
+        uint length = 0;
+        bytes memory string_rep = bytes(str);
+        while (i<string_rep.length) {
+            if (string_rep[i]>>7==0)
+                i += 1;
+            else if (string_rep[i]>>5==0x6)
+                i += 2;
+            else if (string_rep[i]>>4==0xE)
+                i += 3;
+            else if (string_rep[i]>>3==0x1E)
+                i += 4;
+            else
+                //For safety
+                i += 1;
+            length++;
+        }
+        return length;
     }
 }
--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,7 @@
 const web3 = require('./lib.js').getWeb3()
 const abi = require('./abi.js')
-// const address = '0xB8D11d81Bc119B8e0ab1bE0A597ec336C5C19E37'
-const address = '0x226d2FF354af955a7aa1A66E57D739625f7C6135'
+const address = '0x48a83c76FA6BA7Db77373f73E5f0CfD6b812A19D'
+// const address = '0x226d2FF354af955a7aa1A66E57D739625f7C6135'
 
 async function getAccounts() {
 	let accounts = await web3.eth.getAccounts()
