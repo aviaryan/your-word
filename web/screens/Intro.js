0bf54774367970cca5453a2295e43c908aca1e0b
Avi Aryan <avi.aryan123@gmail.com>
2018-09-18 00:06:22 +0530

    add intro

--- web/screens/Intro.js
+++ web/screens/Intro.js
@@ -5,8 +5,24 @@ import styles from './../styles/Intro.css'
 export default class Intro extends Component {
 	render() {
 		return (
-			<div>
-				<h1 className={styles.heading}>Hello there</h1>
+			<div className={styles.help}>
+				<h1>How to use this app?</h1>
+				<p>
+					1. Connect to the <a href="https://www.rinkeby.io/#stats">Rinkeby Test Net</a> using Metamask.
+				</p>
+				<p>
+					2. Load some funds in your test wallet by using the <a href="https://faucet.rinkeby.io/">faucet</a>.
+					Refer this <a href="https://medium.com/compound-finance/95bbbc85fc1d">Medium article</a>.
+				</p>
+				<p>
+					3. You should be able to see the balances on the <a href="/">home page</a> now.
+				</p>
+				<p>
+					4. Post a "word" and bet some money against it.
+				</p>
+				<p>
+					5. If done, mark it SUCCESS otherwise mark it failed.
+				</p>
 			</div>
 		)
 	}
